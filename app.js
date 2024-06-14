6d5c10aec7009d4b900f10bd047c2c974994af3f
Micheal Saviour <saviourme33@gmail.com>
2024-06-14 23:51:38 +0100

    js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var http = require('http');
 var fs = require('fs');
-const port = 8080;
+const port = 9495;
+const host = '0.0.0.0'
 
 var index_html = fs.readFileSync('./index.html');
 var server = http.createServer((req, res) => {
     console.log("[requesting]" + req.url);
-    res.writeHead(200, {'content':'text/html'});
+    res.writeHead(200, {'content-type':'text/html'});
     res.write(index_html);
 
     res.end();
 });
-server.listen(port, () => {
-    console.log("[server initiated] listening for request");
+server.listen(port, host, (port, host) => {
+    console.log("[server http://"+host+":"port+" initiated] listening for request");
 });

1ec87d4fa9964371b70bba8543b0bc36790fc3bd
Micheal Saviour <saviourme33@gmail.com>
2024-06-15 13:42:31 +0100

    commited

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var http = require('http');
 var fs = require('fs');
-const port = 9495;
+const port = process.env.PORT;
 const host = '0.0.0.0'
 
 var index_html = fs.readFileSync('./index.html');
 var server = http.createServer((req, res) => {
-    console.log("[requesting]" + req.url);
+    console.log("[requesting]: " + req.url);
     res.writeHead(200, {'content-type':'text/html'});
     res.write(index_html);
-
     res.end();
 });
 server.listen(port, host);

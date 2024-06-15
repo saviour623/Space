6a8e6435a61bc3658bd47147d541740cd631bba1
Micheal Saviour <saviourme33@gmail.com>
2024-06-15 14:03:47 +0100

    finally deployed first page after hassle

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var fs = require('fs');
 const port = process.env.PORT;
 const host = '0.0.0.0'
 
-var index_html = fs.readFileSync('./index.html');
+var index_html = fs.readFileSync('./login.html');
+var index_css = fs.readFileSync('./login.css');
 var server = http.createServer((req, res) => {
     console.log("[requesting]: " + req.url);
     res.writeHead(200, {'content-type':'text/html'});
     res.write(index_html);
+    res.writeHead(200, {'content-type':'text/css'});
+    res.write(index_css);
     res.end();
 });
 server.listen(port, host);
